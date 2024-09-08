59e56c098fa3453b9f130660c0c139fd14a7e6c4
Abdala <clahimoha1000@gmail.com>
2024-09-08 09:46:37 +0300

    Created an implementation of a Deque using a Doubly Linked List

---
+++ LinkedDeque.py
@@ -0,0 +1,42 @@
+from DoublyLinkedBase import _DoublyLinkedBase
+
+class Empty(Exception):
+    """Error attempting to access an element from an empty container."""
+    pass
+
+class LinkedDeque(_DoublyLinkedBase):    # note the use of inheritance
+    """Double-ended queue implementation based on a doubly linked list."""
+    
+    def first(self):
+        """Return (but do not remove) the element at the front of the deque."""
+        if self.is_empty(self):
+            raise Empty("Deque is empty.")
+        return self._header._next._element      # real item just after header
+    
+    def last(self):
+        """Return (but do not remove) the element at the back of the deque"""
+        if self.is_empty(self):
+            raise Empty("Deque is empty")
+        return self._trailer._prev._element     # real item just before trailer
+    
+    def insert_first(self, e):
+        """Add an element to the front of the deque."""
+        self._insert_between(e, self._trailer._prev, self._trailer) # before trailer
+        
+    def delete_first(self):
+        """Remove and return the element from the front of the deque.
+        
+        Raise Empty exception if the deque is empty.
+        """
+        if self.is_empty(self):
+            raise Empty("Deque is empty")
+        return self._delete_node(self._header._prev)   # use inherited method.
+    
+    def delete_last(self):
+        """Remove and return the element from the back of the deque.
+        
+        Raise Empty exception if the deque is empty.
+        """
+        if self.is_empty(self):
+            raise Empty("Deque is empty")
+        return self._delete_node(self._trailer._prev)   # use inherited method.
